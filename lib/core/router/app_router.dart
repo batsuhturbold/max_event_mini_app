7abd42fcf6264c7666b0dce08062356e4aa68e02
Turbold Batsukh <batsuhturbold@gmail.com>
2025-06-05 15:52:23 +0800

    color variants widgets

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../features/splash/presentation/pages/splash_page.dart';
 
--- lib/shared/constant/app_colors.dart
+++ lib/shared/constant/app_colors.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF4CAF50); // MAX Event primary color
-  static const Color secondary = Color(0xFF2196F3);
-  static const Color background = Color(0xFFF5F5F5);
-  static const Color text = Color(0xFF333333);
+  static const Color primary = Color(0xFF293897); // MAX Event primary color
+  static const Color secondary = Color(0xFFBDC1DF);
+  static const Color third = Color(0xFFDFE1EF);
+  static const Color light = Color(0xFFEAEBF5);
+  static const Color white = Color(0xFFffffff);
+  static const Color black = Color(0xff110e09);
+  static const Color text = Color(0xFF3D3D3D);
+  // ignore: constant_identifier_names
+  static const Color grayhigh = Color(0xFF3D3D3D);
+  static const Color graymedium = Color(0xFFADADAD);
+  static const Color graylow = Color(0xffD6D6D6);
 }
