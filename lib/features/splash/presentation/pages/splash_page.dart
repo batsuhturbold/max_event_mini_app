c58730dd1b8547d42e14d19ec1742f77c13386d4
Turbold Batsukh <batsuhturbold@gmail.com>
2025-06-05 14:15:50 +0800

    splashscreen done

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -6,26 +7,31 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // эсвэл өөр өнгө
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Лого эсвэл зураг
-            Icon(Icons.event, size: 80, color: Colors.blue),
+            // SVG лого
+            SvgPicture.asset(
+              'assets/images/logo.svg',
+              width: 300,
+              height: 67.5,
+              fit: BoxFit.contain,
+            ),
             const SizedBox(height: 24),
             // App нэр
-            Text(
-              'MAX Event App',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            const SizedBox(height: 16),
-            // Loading indicator
-            CircularProgressIndicator(),
+            // Text(
+            //   'MAX test',
+            //   style: TextStyle(
+            //     fontSize: 28,
+            //     fontWeight: FontWeight.bold,
+            //     color: Colors.blue,
+            //   ),
+            // ),
+            // const SizedBox(height: 16),
+            // // Loading indicator
+            // CircularProgressIndicator(),
           ],
         ),
       ),
